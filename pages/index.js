b67a4fe18fe337199b90669b059e1d22ddbc29bf
Gan Tu <github@tugan.dev>
2022-09-10 20:19:44 -0400

    add hero image

--- pages/index.js
+++ pages/index.js
@@ -1,5 +1,6 @@
 import Head from "next/head";
 import Header from "../components/Header";
+import HeroImage from "../components/HeroImage";
 
 export default function Home() {
   return (
@@ -9,6 +10,7 @@ export default function Home() {
         <meta name="description" content="Airbnb UI Clone created by Gan" />
       </Head>
       <Header />
+      <HeroImage />
     </div>
   );
 }
