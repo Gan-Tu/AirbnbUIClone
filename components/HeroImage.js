92d9fef8c606f5733b5f81c978275da3b2785086
Gan Tu <github@tugan.dev>
2022-09-11 14:47:58 -0400

    make object cover

--- components/HeroImage.js
+++ components/HeroImage.js
@@ -6,7 +6,12 @@ function HeroImage() {
   const router = useRouter();
   return (
     <div className="relative h-[300px] sm:h-[400px] lg:h-[500px] xl:h-[600px] 2xl:h-[700px]">
-      <Image src="https://links.papareact.com/0fm" layout="fill" alt="" />
+      <Image
+        src="https://links.papareact.com/0fm"
+        layout="fill"
+        alt=""
+        objectFit="cover"
+      />
       <div className="absolute top-1/2 w-full text-center text-sm sm:text-lg">
         <p>Not sure where to go? Perfect.</p>
         <button
